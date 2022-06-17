e375e3d69048c9b3d0d3c805b329a16e5de7b645
RFKaya <66190943+RFKaya@users.noreply.github.com>
2022-06-17 18:23:01 +0300

    Add files via upload

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const db = require("quick.db");
+module.exports = member => {
+
+    let öncedenKatılmaDurumu = db.fetch(`ilkKatılmaTarihi_${member.user.id}`);
+    let kayıtBilgi = db.fetch(`Uye_${member.user.id}`);
+
+    if (member.guild.id == "746357184211714089") {
+        if ((Date.now() - member.user.createdAt.getTime()) > 31557600000) {
+            if (!öncedenKatılmaDurumu && !kayıtBilgi) {
+                member.roles.add("813366567475216403")
+                db.set(`ilkKatılmaTarihi_${member.user.id}`, Date.now())
+                db.set(`Uye_${member.user.id}`, Date.now());
+                db.set(`UyeYetkili_${member.user.id}`, "815192339688456222");
+            }
+        }
+    }
+
+};
