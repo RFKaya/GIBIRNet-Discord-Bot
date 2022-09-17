396134db5ca3e00268395f346c328da72b1c0659
RFKaya <66190943+RFKaya@users.noreply.github.com>
2022-09-18 00:18:15 +0300

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -2,6 +2,7 @@ const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('messageCreate', reqEvent('messageCreate'));
+  client.on('interactionCreate', reqEvent('interactionCreate'));
   client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
   client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
 };
